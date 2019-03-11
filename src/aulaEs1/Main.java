ba85a1cdb3bb89cdf5577e16f3bafc81dcf02a11
Andre Ricardo <andre-bala13@hotmail.com>
2019-03-11 10:11:19 -0300

    commit

---
+++ src/aulaEs1/Main.java
@@ -0,0 +1,37 @@
+package aulaEs1;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+
+
+import com.lowagie.text.Document;
+import com.lowagie.text.DocumentException;
+import com.lowagie.text.Paragraph;
+import com.lowagie.text.pdf.PdfWriter;
+
+public class Main {
+
+	public void createPDF() {
+		String fileName = "teste";//JOptionPane.showInputDialog("Nome do arquivo")
+		String pathFile = "/home/alunos/eclipse-workspace/aula es1/" + fileName;
+		
+		Document document = new Document ();
+		try {
+			PdfWriter.getInstance(document,	new FileOutputStream(pathFile + ".pdf"));
+			document.open();
+			document.add(new Paragraph ("Exemplo simples"));
+			document.close();
+			// JOptionPane.showMessageDialog (null, "Arquivo salvo com sucesso")
+			
+		} catch (DocumentException de) {
+			System.err.println(de.getMessage());
+		} catch(IOException ioe) {
+			System.err.println(ioe.getMessage());
+		}
+	}
+	public static void main (String[] args) {
+		Main m = new Main();
+		m.createPDF();
+	}
+}
